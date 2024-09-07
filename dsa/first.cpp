1cd8a93090aa8dee8ed021266c3f3b4de36df063
Aryaveer kanwar <167698169+AryaVeer0505@users.noreply.github.com>
2024-09-07 18:41:21 +0530

    Add files via upload

---
+++ dsa/first.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+using namespace std;
+int main(){
+	  
+//Day===1 
+//(flowcharts,psedocodes,basic questions) 
+
+//max of two numbers 
+
+//int a,b;
+//cout<<"Enter value of a:";
+//cin>>a;
+//cout<<"Enter value of b:";
+//cin>>b;
+//if(a>b){
+//	cout<<"a is greater";
+//}
+//else{
+//	cout<<"b is greater";
+//}
+
+//sum of n numbers 
+
+//int n;
+//int sum=0;
+//cout<<"Enter value of n:";
+//cin>>n;
+//for(int i=0;i<=n;i++){
+//   sum=sum+i;
+//}
+//cout<<"Sum of n numbers:"<<sum;
+
+//	age
+
+//    int age=20;
+//    if(age>=18){
+//        cout<<"can vote";
+//    }
+//    else{
+//        cout<<"can not vote";
+//    }
+
+//Calculate Simple interest 
+
+//     int principle=2000;
+//     int rate=20;
+//     int time=24;
+//     int SI=principle*rate*time;
+//     cout<<SI;
+
+//factorial 
+
+//
+//int n=5;
+//int fact=1;
+//for(int i=1;i<=n;i++){
+//	fact=fact*i;
+//
+//}
+//	cout<<fact;
+
+
+//Day===2
+//(variables,datatypes,operators,questions) 
+
+//size of operator 
+//int age=20;
+//cout<<sizeof(age);
+
+//datatypes 
+
+//int===4bytes(1byte==8bits)
+//    int age=20;
+//    cout<<sizeof(age);
+
+//char===1bytes
+//   char name='a';
+//   cout<<sizeof(name);
+//string===8bytes
+//   string name="aryaveer";
+//   cout<<sizeof(name);
+//float===4
+//     float percentage=90.20;
+//     cout<<sizeof(percentage);
+//double===8
+//     double percentage=90.20;
+//     cout<<sizeof(percentage);
+//bool===1bit(true or false)
+       bool a=true;
+       cout<<a;
+
+
+
+return 0;
+}
