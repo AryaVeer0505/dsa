e34dcda52955dbc31590ce520cb8f58420ed6571
Aryaveer kanwar <167698169+AryaVeer0505@users.noreply.github.com>
2024-09-21 16:14:22 +0530

    Add files via upload

--- dsa/first.cpp
+++ dsa/first.cpp
@@ -783,6 +783,62 @@ int main(){
 //	cout<<endl;
 //}
 
+//7=== hollow diamond pattern 
+
+//int n=4;
+//for(int i=0;i<n;i++){
+//	for(int j=0;j<n-i-1;j++){
+//		cout<<" ";
+//	}
+//	cout<<"*";
+//
+//if(i!=0){
+//
+//		for(int j=0;j<2*i-1;j++){
+//			cout<<" ";
+//		}
+//		cout<<"*";
+//	}
+//	cout<<endl;
+//}
+//for(int i=0;i<(n-1);i++){
+//	for(int j=0;j<i+1;j++){
+//		cout<<" ";
+//	}
+//	cout<<"*";
+//	if(i!=n-2){
+//		for(int j=0;j<(2*(2-i)-1);j++){
+//			cout<<" ";
+//		}
+//		cout<<"*";
+//	}
+//	cout<<endl;
+//}
+
+//8==Butterfly pattern
+
+//int n=4;
+//for(int i=0;i<n;i++){
+//	for(int j=0;j<i+1;j++){
+//	cout<<"*";
+//	}
+//	if(i!=0){
+//	
+//	for(int j=0;j<2*(n-2*i);j++){
+//		cout<<" ";
+//	}
+//}
+//
+//	for(int j=0;j<i+1;j++){
+//	cout<<"*";
+//	}
+//	
+//	cout<<endl;
+//}
+
+
+//day ===5
+//(functions,basic questions)
 
 return 0;
 }
